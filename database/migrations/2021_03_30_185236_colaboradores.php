4faf7148f9a1a6ab41e0e4ea1786e95f66d7d300
annaHdez <ana.ferhero@gmail.com>
2021-03-30 13:10:30 -0600

    Actualización 1

---
+++ database/migrations/2021_03_30_185236_colaboradores.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Colaboradores extends Migration
+{
+        public function up()
+    {
+        /*Schema::create('colaboradores', function (Blueprint $table){
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('apellido_paterno', 100);
+            $table->string('apellido_materno', 100);
+            $table->char('sexo'); //Masculino, Femenino
+            fecha nacimiento
+            educación
+            fecha admisión
+            área
+            posición
+            sueldo
+            SD IMSS
+            SDI
+            fecha baja
+            antiguedad
+        });*/
+    }
+
+        public function down()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*Route:get('/cache', function() {
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    Artisan::call('cache:clear');
+    return "Caché limpio";
+})->name('cache');*/
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('users', 'UserController');
+
